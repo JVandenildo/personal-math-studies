1afd5aa19a4bd0b4c03ab24407f911a72786d936
JVandenildo <jvandenildo@gmail.com>
2023-04-10 18:47:06 -0300

    factorial using js

---
+++ Factorial/factorial.js
@@ -0,0 +1,15 @@
+function factorial(number){
+    let i = 1;
+    var result = 1;
+
+    while (number >= i){
+        result = result * i;
+        i += 1;
+    }
+
+    return result;
+}
+
+var number = 5; // just for testing, 5! is 120
+
+console.log(`${number}! is ${factorial(number)}.`);
