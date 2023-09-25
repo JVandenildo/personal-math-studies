081c2a119e8c930370411c6ead88109cd50ae11e
J. Vandenildo <jvandenildo@gmail.com>
2023-09-25 09:46:02 -0300

    numpy mean for comparison

--- Statistic/mean.py
+++ Statistic/mean.py
@@ -1,4 +1,5 @@
 import random as rd
+import numpy as np
 
 
 def meanOfLst(array):
@@ -11,4 +12,7 @@ def meanOfLst(array):
 
 lst = [rd.randint(1, 100) for x in range(rd.randint(5, 15))]
 
+# result got by function
 print(f"List: {lst};\nLength: {len(lst)};\nMean: {meanOfLst(lst)}.")
+# result got by numpy
+print(f"Numpy mean: {round(np.mean(lst), 2)}.")
