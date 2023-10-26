e9b304e679e7ee4464eb072ffb464a5544ccfacc
J. Vandenildo <jvandenildo@gmail.com>
2023-10-26 13:29:30 -0300

    correction

--- Statistic/median.py
+++ Statistic/median.py
@@ -7,10 +7,7 @@ import numpy as np
 
 
 def medianOfLst(array: list) -> float:
-    # j = 0
-    # k = 0
     if len(array) % 2 == 0:  # array length is even
-        # return array
         while len(array) > 2:
             array.remove(max(array))
             array.remove(min(array))
@@ -18,27 +15,17 @@ def medianOfLst(array: list) -> float:
         return (array[0] + array[1]) / 2
 
     else:  # array length is odd
-        # while j < len(array):
-        #     k = array[j]
-
-        #     if k < array[j] and k > array[j + 1]:
-        #         array.remove(array[j])
-        #         array.remove(array[j + 1])
-        #     else:
-        #         array.remove(k)
-        #     j = j + 1
-
-        # return k
         while len(array) > 1:
             array.remove(max(array))
             array.remove(min(array))
 
-        return array[0]
+    return array[0]
 
 
 lst = [rd.randint(1, 100) for i in range(rd.randint(5, 20))]
+print(f"List: {lst}\nLength: {len(lst)}.")
 
 # result by numpy for comparison
-print(f"List: {lst}\nNumpy median: {np.median(lst)}.")
+print(f"Numpy median: {np.median(lst)}.")
 # result by function
-print(f"List: {lst};\nLength: {len(lst)};\nMedian: {medianOfLst(lst)}.")
+print(f"My median: {medianOfLst(lst)}.")
