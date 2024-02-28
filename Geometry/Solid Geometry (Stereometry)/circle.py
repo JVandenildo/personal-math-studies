cd64489283ec85ed312ac1c01789e0351673ac4e
J. Vandenildo <jvandenildo@gmail.com>
2024-02-28 17:31:40 -0300

    diameter correction and circumference

--- Geometry/Solid Geometry (Stereometry)/circle.py
+++ Geometry/Solid Geometry (Stereometry)/circle.py
@@ -5,12 +5,17 @@ class circle:
     def __init__(self, radius):
         self.radius = radius
 
+    def diameter(self):
+        return round(2 * self.radius, 2)
+
     def area(self):
         return round(pi * (self.radius**2), 2)
 
-    def diameter(self):
-        return round(pi * self.radius, 2)
+    def circumference(self):
+        return round(2 * pi * self.radius, 2)
 
 
 NeoCircle = circle(float(input("Radius: ")))
-print(f"Area: {NeoCircle.area()};\nDiameter: {NeoCircle.diameter()}.")
+print(
+    f"Diameter: {NeoCircle.diameter()};\nArea: {NeoCircle.area()};\nCircumference: {NeoCircle.circumference()}."
+)
